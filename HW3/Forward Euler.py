69f8b9e33b584657f885cc869f5d9247b6e60c73
Amy Manning <amycurly.manning@gmail.com>
2023-10-09 13:32:03 -0500

    write simple script to start hw

---
+++ HW3/Forward Euler.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plot
+t = [-1, 0, 0.00001, 0.01, 1, 2, 5.5, 100000000]
+alpha = int(3)
+#dydt = -1 * alpha * y
+for i in t:
+    y = np.exp(-1*alpha * i) # Exact Solution
+    yprime = (1-alpha*i)*y # Update Equation
+    print(y)
+    print(yprime)
+# graph
+
+
