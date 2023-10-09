d0336233d5332276537b09ebfb181e1a66f771e8
Amy Manning <amycurly.manning@gmail.com>
2023-10-08 21:12:28 -0500

    remove remote

---
+++ Project 1/svd checker.py
@@ -0,0 +1,14 @@
+# Singular-value decomposition
+from numpy import array
+from numpy.linalg import svd
+# define a matrix
+A = array([[1, 2], [3, 4]])
+#print(A)
+# SVD
+U, s, VT = svd(A)
+print('U = ')
+print(U)
+print('sigma = ')
+print(s)
+print('VT = ')
+print(VT)
