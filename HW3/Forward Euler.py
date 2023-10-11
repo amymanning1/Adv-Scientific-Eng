582724e594fbfeafabbaa300f2f37761df842a7f
Amy Manning <amycurly.manning@gmail.com>
2023-10-11 08:44:00 -0500

    working on plotting solutions

--- HW3/Forward Euler.py
+++ HW3/Forward Euler.py
@@ -1,13 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plot
-t = [-1, 0, 0.00001, 0.01, 1, 2, 5.5, 100000000]
+t = [-1, 0, 0.00001, 0.01, 1, 1.0000006, 5.5, 100000000]
 alpha = int(3)
 #dydt = -1 * alpha * y
+ylist=[]
 for i in t:
     y = np.exp(-1*alpha * i) # Exact Solution
     yprime = (1-alpha*i)*y # Update Equation
-    print(y)
-    print(yprime)
+    # print(y)
+    # print(yprime)
+
 # graph
 
 
