daee2c4fa7245b12949db5c6422119b7d18807d3
Amy Manning <amycurly.manning@gmail.com>
2023-11-28 22:13:42 -0600

    Work on graphing FE more

--- CG-FEM-Heat-Eqn/Heat_GalerkinFEM.py
+++ CG-FEM-Heat-Eqn/Heat_GalerkinFEM.py
@@ -26,55 +26,96 @@ def nontriv_mat(N,Ne,h):
     K=K/h
     # Now building reduced versions
     twos=(Ne-1)*[2]
-    ones=np.ones(Ne-2)
-    M_red=(np.diag(twos)+np.diag(ones,k=-1)+np.diag(ones,k=1))*(h/6)
+    ones=(Ne-2)*[1]
+    M_red=(np.diag(twos)+np.diag(ones,k=-1)+np.diag(ones,k=1))
     M_inv=LA.inv(M_red) # M_inv is inverse of reduced M matrix
-    K_red=(np.diag(twos)+np.diag(-1*ones,k=-1)+np.diag(-1*ones,k=1))/6
-    return M,M_red,K,K_red,M_inv
+    K_red=(np.diag(twos)+np.diag(-1*ones,k=-1)+np.diag(-1*ones,k=1))
+    K_red=K_red/h
+    return K_red,M_inv
 
-def gridMap(N,Ne,xl,xr,h,x):
+def gridMap(Ne,xl,xr,h,x):
     i=0
     iee=np.zeros((Ne,2))
     while i<Ne:
-        x[i]=xl+(i-1)*h
+        x[i]=xl+(i)*h
         iee[i][0]=i
         iee[i][1]=i+1
         i+=1
+    x[Ne]=xr
     return x,iee
 
 def dbc(x):
     ux=np.sin(np.pi*x)
     return ux
 
-def forEul(nt,t0,dt,Ne,h,q1,q2,iee,x,M_inv,K_red,x_red,M,K,N):
+def forEul(nt,t0,dt,Ne,h,iee,x,M_inv,K_red,x_red):
     # Used quadrature to integrate F, work is in pdf
     # Now performing local element calculations
     n=1
-    k=0
-    l=0
-    F=np.zeros(Ne-1)
-    # F=np.zeros(N)
+    
+    dx=h/2
     u=dbc(x_red)
-    # u=dbc(x)
     while n<=nt:
+        F=np.zeros(Ne)
         ctime=t0+n*dt
-        n+=1
+        k=0
         # Build time dependent RHS vector
-        while k<Ne: # loop over grid elements
+        while k<Ne-1: # loop over grid elements
             # 2 local nodes for every 1D element, 2nd order gaussian quadrature
+            q1=1/m.sqrt(3) # Quadrature points, weights=1 in second order quadrature; if user chooses a higher order, initialize weights and intro in equation; here they are redundant
+            q2=-1/m.sqrt(3)
+            q1map=(q1+1)*dx+x[k+1]
+            q2map=(q2+1)*dx+x[k+1]
             flocal=[0,0]
-            flocal[0]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi1(q1)*np.sin(np.pi*q1)+phi1(q2)*np.sin(np.pi*q2))
-            flocal[1]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi2(q1)*np.sin(np.pi*q1)+phi2(q2)*np.sin(np.pi*q2))
-
+            flocal[0]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi1(q1)*np.sin(np.pi*q1map)+phi1(q2)*np.sin(np.pi*q2map))
+            flocal[1]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi2(q1)*np.sin(np.pi*q1map)+phi2(q2)*np.sin(np.pi*q2map))
+            
             # Finite Element Assembly
+            l=0
             while l<2:
                 global_node1=iee[k][l]
                 F[int(global_node1)]+=flocal[l]
                 l+=1
             k+=1
         # forward euler
-        u=u-dt*M_inv@K_red@u+dt*M_inv@F
-        # u=u-dt*LA.inv(M)@K@u+dt*LA.inv(M)@F
+        F_red=F[0:-1]
+        u=u-dt*((1/6)*(M_inv@K_red))@u+dt*(M_inv*(h/6))@F_red
+        n+=1
+    return u
+
+def backEul(nt,t0,dt,Ne,h,iee,x,M_inv,K_red,x_red):
+        # Used quadrature to integrate F, work is in pdf
+    # Now performing local element calculations
+    n=1
+    
+    dx=h/2
+    u=dbc(x_red)
+    while n<=nt:
+        F=np.zeros(Ne)
+        ctime=t0+n*dt
+        k=0
+        # Build time dependent RHS vector
+        while k<Ne-1: # loop over grid elements
+            # 2 local nodes for every 1D element, 2nd order gaussian quadrature
+            q1=1/m.sqrt(3) # Quadrature points, weights=1 in second order quadrature; if user chooses a higher order, initialize weights and intro in equation; here they are redundant
+            q2=-1/m.sqrt(3)
+            q1map=(q1+1)*dx+x[k+1]
+            q2map=(q2+1)*dx+x[k+1]
+            flocal=[0,0]
+            flocal[0]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi1(q1)*np.sin(np.pi*q1map)+phi1(q2)*np.sin(np.pi*q2map))
+            flocal[1]=(np.pi**2-1)*np.exp(-ctime)*(h/2)*(phi2(q1)*np.sin(np.pi*q1map)+phi2(q2)*np.sin(np.pi*q2map))
+            
+            # Finite Element Assembly
+            l=0
+            while l<2:
+                global_node1=iee[k][l]
+                F[int(global_node1)]+=flocal[l]
+                l+=1
+            k+=1
+        # forward euler
+        F_red=F[0:-1]
+        u_next=u-dt*((1/6)*(M_inv@K_red))@u+dt*(M_inv*(h/6))@F_red # left off here
+        n+=1
     return u
 
 def main():
@@ -86,25 +127,22 @@ def main():
     h=(xr-xl)/Ne
     x=np.linspace(xl,xr,N)
     x_red=x[1:-1]
-    u_x=np.sin(np.pi*x) # u(x,0)=sin(pi*x)
-    u_0=0 # u(0,t)=u(1,t)=0
-    t0=0
+    t0=0 #time constants
     tf=1
     dt=1/551
-    dc=2/h
-    dx=h/2
     nt=(tf-t0)/dt
-    q1=1/m.sqrt(3) # Quadrature points, weights=1 in second order quadrature; if user chooses a higher order, initialize weights and intro in equation; here they are redundant
-    q2=-1/m.sqrt(3)
     
-    M,M_red,K,K_red,M_inv=nontriv_mat(N,Ne,h)
-    x,iee=gridMap(N,Ne,xl,xr,h,x)
-    u=forEul(nt,t0,dt,Ne,h,q1,q2,iee,x,M_inv,K_red,x_red,M,K,N)
+    # Call Functions
+    K_red,M_inv=nontriv_mat(N,Ne,h)
+    x,iee=gridMap(Ne,xl,xr,h,x)
+    u=forEul(nt,t0,dt,Ne,h,iee,x,M_inv,K_red,x_red)
+    v=backEul(nt,t0,dt,Ne,h,iee,x,M_inv,K_red,x_red)
     # Add natural bcs to u
     nat0=0 # u(0,t)=u(1,t)=0
     nat1=0
     u=np.append(u,nat1)
     u=np.insert(u,0,nat0)
+    print('u = ')
     print(u)
 
     # Plotting
@@ -113,6 +151,7 @@ def main():
     plt.plot(x,exact_sol,color='red',label='Exact Solution')
     plt.plot(x,u,color='blue',label='FEM estimate')
     plt.title('Forward Euler with timestep ' + str(dt))
+    plt.legend()
     plt.show()
 
 if __name__ == '__main__':
